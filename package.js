4113c14947817361956c74315c5ef0e3f8a85c30
skwasha <sascha@umich.edu>
2016-04-25 23:05:17 -0700

    downrev from 1.3 requirement

--- package.js
+++ package.js
@@ -2,13 +2,13 @@ Package.describe({
   summary: "Allows you to define and run scheduled jobs across multiple servers.",
   version: "1.3.2",
   name: "hellotech:synced-cron",
-  git: "https://github.com/hellotech/meteor-synced-cron.git"
+  git: "https://github.com/HelloTech/meteor-synced-cron.git"
 });
 
 Npm.depends({later: "1.1.6"});
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.3');
+  api.versionsFrom('METEOR@1.2');
   api.use(['underscore', 'check', 'mongo', 'logging'], 'server');
   api.add_files(['synced-cron-server.js'], "server");
   api.export('SyncedCron', 'server');
