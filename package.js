af09467529143879dc4142491c04ab20f6c70902
skwasha <sascha@umich.edu>
2016-04-25 23:15:56 -0700

    hmmm

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
   summary: "Allows you to define and run scheduled jobs across multiple servers.",
   version: "1.3.2",
-  name: "hellotech:synced-cron",
-  git: "https://github.com/HelloTech/meteor-synced-cron.git"
+  name: "hellotech:meteor-synced-cron",
+  git: ""
 });
 
 Npm.depends({later: "1.1.6"});
